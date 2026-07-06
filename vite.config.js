83c2153a2fe7034060cf964203a9c8e2b2dd0409
Dieval <36481844+DarkDieval@users.noreply.github.com>
2026-07-06 12:25:25 -0500

    Change base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/web_project_around_react/",
+  base: "/web_project_around_react_sprint14/",
   plugins: [react()],
   server: {
     port: 3000,
